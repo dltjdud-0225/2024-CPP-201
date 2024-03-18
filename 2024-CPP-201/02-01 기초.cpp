27dcb14459e8db91b2d419fb8366104094f24e5d
dltjdud-2025 <s2331@e-mirim.hs.kr>
2024-03-18 14:28:07 +0900

    C++기본형태

---
+++ 2024-CPP-201/02-01 기초.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+
+
+void main(void) 
+{
+	//std : 이름 공간(name space)
+	//cout : 출력 객체
+	//endl : 줄바꿈(개행) 객체
+	std::cout << "HELLO WORLD" << std::endl;
+	
+}
--- 2024-CPP-201/test.cpp
+++ 2024-CPP-201/test.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
+	//학교가 가기 싫다는 말을 
 	printf("학교? 그게 뭐죠? 먹는 건가요:)");
 }
