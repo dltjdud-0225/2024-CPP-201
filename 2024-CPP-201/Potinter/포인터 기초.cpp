83daa6177629adcabafafb89ab11af60f6747486
dltjdud-2025 <s2331@e-mirim.hs.kr>
2024-03-22 10:02:21 +0900

    배열의 이름은 시작주소값을 의미

---
+++ 2024-CPP-201/Potinter/포인터 기초.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	int num = 3;
+	cout << num << endl;		//3
+	cout << &num << endl;	//변수가 메모리 공간에 할당되어 있는 주소
+
+	int arr[5] = { 10, 20, 30, 40, 50 };
+	cout << arr << endl;		//배열이 메모리 공간에 할당죄어 있는 주소
+
+	return 0;
+
+}
+
