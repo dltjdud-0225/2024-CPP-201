0e8db527856ad9b8684657cc5839381b280696cb
dltjdud-2025 <s2331@e-mirim.hs.kr>
2024-04-08 13:53:05 +0900

    주영 문자열 비교 - C++스타일

---
+++ 2024-CPP-201/02-05문자열 비교.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+void main(void)
+{
+	string str1 = "juyoung";
+	//문장열 비교
+	if (str1 == "juyoung")
+		cout << "같다";
+	else
+		cout << "다르다";
+
+}
