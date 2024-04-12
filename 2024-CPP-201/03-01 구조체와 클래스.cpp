afba300f704e88d877aa90e8d9cf9d698fb216c6
dltjdud-2025 <s2331@e-mirim.hs.kr>
2024-04-12 10:02:42 +0900

    동적할당

--- 2024-CPP-201/03-01 구조체와 클래스.cpp
+++ 2024-CPP-201/03-01 구조체와 클래스.cpp
@@ -7,15 +7,15 @@ class Student {
 public:
 	// 생성자 : 객체를 생성할 때 호출되는 함수
 	// 생성자는 반환형이 없다.
-	Student(int hakbun, string name) {
-		hakbun_ = hakbun;
-		name_ = name;
+	Student(int hakbun, string name)
+		: hakbun_(hakbun), name_(name)
+	{
 	}
 
 	//생성자 오버로딩
-	Student() {
-		hakbun_ = 2121;
-		name_ = "JWP";
+	Student() 
+		: hakbun_(2121), name_("JWP")
+	{
 	}
 
 	//객체의 멤버를 출력하는 함수
@@ -29,10 +29,14 @@ private:
 };
 
 void main(void)
-{
-	Student juyoung = Student(2115, "윤주영");
+{	//동적 할당 : heap 영역에 할당. 실행시간(run time)에 메모리 크기 결정
+	Student* juyoung = new Student(2115, "윤주영");
+	//정적 할당 : stack 영역에 할당. 컴파일 시간에 메모리 크기 결정
 	Student jwp = Student();
 
-	juyoung.print();
+	(*juyoung).print();
 	jwp.print();
+
+	//동적할당된 메모리 해제
+	delete juyoung;
 }
