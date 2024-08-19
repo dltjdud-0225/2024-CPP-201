5f7c0343c8a024b1fc4e739cdbf72fea324f013e
dltjdud-2025 <s2331@e-mirim.hs.kr>
2024-08-19 15:06:02 +0900

    정적바인딩

--- 2024-CPP-201/05-01다형성.cpp
+++ 2024-CPP-201/05-01다형성.cpp
@@ -1,49 +1,80 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
 class Animal {
-public: // 메서드를 public으로 변경
-    Animal(string name, unsigned int age)
-        : name_(name), age_(age) {
-        cout << "이름" << name_ << endl;
-        cout << "나이" << age_ << endl;
+public:      // 멤버 함수
+    Animal() {
+
     }
+
+    Animal(string name, unsigned int age)
+        : name_(name), age_(age)
+    {
+        cout << "이름 " << name_ << endl;
+        cout << "나이 " << age_ << endl;
+    }
+
     void bark() {
-        cout << "잘 짖는다" << endl;
+        cout << "잘 짖는다." << endl;
     }
 
     void sleep() {
-        cout << "잘 잔다" << endl;
+        cout << "잘 잔다." << endl;
     }
 
     void eat() {
-        cout << "잘 먹는다" << endl;
+        cout << "잘 먹는다." << endl;
     }
 
-private:
+private:      // 멤버 변수
     string name_;
-    unsigned int age_;
+    unsigned int age_;      // 음의 부호가 없는 int
+
 };
 
-class human : public Animal {
+class Human : public Animal {
 public:
+    // 부모(Animal) 생성자가 먼저 호출된다.
+    Human(string name, unsigned int age, bool right)
+        : Animal(name, age), right_(right)
+    {
+        cout << "인권존재여부 " << right_ << endl;
+    }
+
+    //TODO : 정적 바인딩을 동적바인딩으로 고치기
+    void bark() {
+        cout << "톡톡" << endl;
+    }
+
+    void sleep() {
+        cout << "쿨쿨" << endl;
+    }
+
+    void eat() {
+        cout << "냠냠" << endl;
+    }
 
 private:
     bool right_;
 };
 
-
-int main() { // 반환형을 int로 변경
-    Animal* ani = new Animal("정민레이디", 18);
-
+void main(void)
+{
+    Animal* ani = new Animal("정민레이디", 18);      // 동적할당
     ani->bark();
-    ani->sleep();
     ani->eat();
+    ani->sleep();
+    delete ani;      // 할당 해제
 
+    //ani의 자료형은 Animal*
+    ani = new Human("지혜레이디", 18, true);      // 동적할당
+    //정적 바인딩으로 인해 Animal의 멤버함수가 호출된다.
+    ani->bark();
+    ani->eat();
+    ani->sleep();
 
+    delete ani;      // 할당 해제
 
-    delete ani;
-
-    return 0; // int 반환형이므로 return 0; 추가
 }
