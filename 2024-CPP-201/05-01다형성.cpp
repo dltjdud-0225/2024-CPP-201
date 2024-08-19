4f818ae410a6e8182732273a913bd665fa4318e1
dltjdud-2025 <s2331@e-mirim.hs.kr>
2024-08-19 13:27:48 +0900

    동적할당 복습

---
+++ 2024-CPP-201/05-01다형성.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+class Aniaml{
+public:
+	void bark()
+	{
+
+	}
+
+	void sleep()
+	{
+
+	}
+	
+	void eat()
+	{
+
+	}
+
+private:
+	string name;
+	unsigned int age;
+	
+};
+
+void main(void)
+{
+	//동적할당
+	Aniaml* ani = new Aniaml();
+	delete animal;
+}
