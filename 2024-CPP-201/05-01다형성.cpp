10d5a19b9d354b8bce6efe9ecdba743f23e0eda6
dltjdud-2025 <s2331@e-mirim.hs.kr>
2024-08-19 14:25:49 +0900

    생성자 복습

--- 2024-CPP-201/05-01다형성.cpp
+++ 2024-CPP-201/05-01다형성.cpp
@@ -1,40 +1,49 @@
 #include<iostream>
 #include<string>
-
 using namespace std;
 
+class Animal {
+public: // 메서드를 public으로 변경
+    Animal(string name, unsigned int age)
+        : name_(name), age_(age) {
+        cout << "이름" << name_ << endl;
+        cout << "나이" << age_ << endl;
+    }
+    void bark() {
+        cout << "잘 짖는다" << endl;
+    }
 
-class Animal{
-public:
-	void bark()
-	{
-		cout << "잘 짖는다" << endl;
-	}
+    void sleep() {
+        cout << "잘 잔다" << endl;
+    }
 
-	void sleep()
-	{
-		cout << "잘 잔다" << endl;
-	}
-	
-	void eat()
-	{
-		cout << "잘 먹는다" << endl;
-	}
+    void eat() {
+        cout << "잘 먹는다" << endl;
+    }
 
 private:
-	string name;
-	unsigned int age;
-	
+    string name_;
+    unsigned int age_;
 };
 
-void main(void)
-{
-	Animal* ani = new Animal();
-	ani->bark();
-	ani->eat();
-	ani->sleep();
+class human : public Animal {
+public:
 
-	delete ani;
+private:
+    bool right_;
+};
 
 
+int main() { // 반환형을 int로 변경
+    Animal* ani = new Animal("정민레이디", 18);
+
+    ani->bark();
+    ani->sleep();
+    ani->eat();
+
+
+
+    delete ani;
+
+    return 0; // int 반환형이므로 return 0; 추가
 }
