18f9516cd062a3017bdf7e1c7a8793c5c6dbb427
dltjdud-2025 <s2331@e-mirim.hs.kr>
2024-09-27 08:53:40 +0900

    09-01 enum 적용 전

---
+++ 2024-CPP-201/09-01 enum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void mmain(void) {
+	// 0 : 한식, 1 : 일식, 2 : 중식
+	int style = 0;
+
+	switch (style) {
+	case 0 : 
+		cout << "한식" << endl;
+		break;
+	case 1 :
+		cout << "일식" << endl;
+		break;
+	case 2:
+		cout << "중식" << endl;
+		break;
+
+	}
+}
