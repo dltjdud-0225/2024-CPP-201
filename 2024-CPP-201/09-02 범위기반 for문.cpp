e046a72860084a0b65ebc0c6908597213fe48847
dltjdud-2025 <s2331@e-mirim.hs.kr>
2024-09-30 13:56:37 +0900

    09-02 범위기반 적용 후

--- 2024-CPP-201/09-02 범위기반 for문.cpp
+++ 2024-CPP-201/09-02 범위기반 for문.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 int main() {
     vector<int> arr = {201, 202, 203, 204};
-    for (int i = 0; i < arr.size(); i++)
-        cout << arr[i] << " ";
+    for (int x : arr)
+        cout << x << " ";
 }
