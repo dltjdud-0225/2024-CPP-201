a271bba92580e7083d490d058a4af6a7b3cc8f30
dltjdud-2025 <s2331@e-mirim.hs.kr>
2024-10-14 13:42:28 +0900

    09-03 auto 적용 전

--- 2024-CPP-201/09-02 범위기반 for문.cpp
+++ 2024-CPP-201/09-02 범위기반 for문.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include<string>
 #include <vector>
 
 using namespace std;
 
 int main() {
-    vector<int> arr = {201, 202, 203, 204};
-    for (int x : arr)
-        cout << x << " ";
+    int a = 40;
+    cout << a << endl;
+
+    vector<string> v = { "백종원","안성재","트리플스타"};
+    string b = v[2];
+    cout << v[2] << endl;
 }
