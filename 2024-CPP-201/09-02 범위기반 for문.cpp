c718ef3573fd79fbbcd84303bd5b9dd3c9ec5ae1
dltjdud-2025 <s2331@e-mirim.hs.kr>
2024-10-14 13:48:55 +0900

    09-03 auto 적용 후

--- 2024-CPP-201/09-02 범위기반 for문.cpp
+++ 2024-CPP-201/09-02 범위기반 for문.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 int main() {
-    int a = 40;
+    auto a = 40;
     cout << a << endl;
 
     vector<string> v = { "백종원","안성재","트리플스타"};
-    string b = v[2];
-    cout << v[2] << endl;
+    auto b = v[2];
+    cout << b << endl;
 }
