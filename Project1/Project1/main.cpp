5657977a3d283289411a3bc51b0ec97155ba323e
dltjdud-2025 <s2331@e-mirim.hs.kr>
2024-10-21 13:49:55 +0900

    [pj] 플레이어 이동

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,40 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+enum App {
+    WIDTH = 1200,
+    HEIGHT = 800
+};
+
+void main(void) {
+    RenderWindow window(VideoMode(App::WIDTH, App::HEIGHT), "Project");
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        RectangleShape player;
+        player.setSize(Vector2f(200.f, 200.f));
+        player.setPosition((App::WIDTH - player.getSize().x) / 2, (App::HEIGHT - player.getSize().y) / 2);
+        player.setFillColor(Color::Magenta);
+
+        if (Keyboard::isKeyPressed(Keyboard::Key::Right))
+            player.move(10.f, 0.f);
+        if (Keyboard::isKeyPressed(Keyboard::Key::Left))
+            player.move(-10.f, 0.f);
+        if (Keyboard::isKeyPressed(Keyboard::Key::Up))
+            player.move(0.f, -10.f);
+        if (Keyboard::isKeyPressed(Keyboard::Key::Down))
+            player.move(0.f, 10.f);
+
+        window.clear(Color::White);
+        window.draw(player);
+        window.display();
+    }
+};
